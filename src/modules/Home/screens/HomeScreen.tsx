540fe19fd609ebe26b74e73cfaf74d4598905634
usaif1 <saif.ullah@fuelbuddy.in>
2024-07-18 02:58:16 +0530

    added r3f and r3f/drei

--- src/modules/Home/screens/HomeScreen.tsx
+++ src/modules/Home/screens/HomeScreen.tsx
@@ -1,7 +1,19 @@
+import { Canvas } from "@react-three/fiber";
 import React from "react";
 
 const HomeScreen: React.FC = () => {
-  return <div>This is the home Screen</div>;
+  return (
+    <div>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 };
 
 export default HomeScreen;
